c1603395546f442aea3fe78ecb9c9ecf6b794b15
lowjichow <phucloidanghoconline@gmail.com>
2025-10-01 01:27:25 +0700

    add dto

---
+++ services/auth-service/src/main/java/com/roomie/services/auth_service/dto/request/AuthRequest.java
@@ -0,0 +1,14 @@
+package com.roomie.services.auth_service.dto.request;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class AuthRequest {
+    String username;
+    String password;
+}
---
+++ services/auth-service/src/main/java/com/roomie/services/auth_service/dto/request/UserUpdateRequest.java
@@ -0,0 +1,16 @@
+package com.roomie.services.auth_service.dto.request;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class UserUpdateRequest {
+    String password;
+    List<String> roles;
+}
