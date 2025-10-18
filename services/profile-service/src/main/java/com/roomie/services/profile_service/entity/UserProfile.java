2638e2b6fae1983d2ef9c8818748041189e5f445
lowjichow <phucloidanghoconline@gmail.com>
2025-10-18 22:34:06 +0700

    add entity

---
+++ services/profile-service/src/main/java/com/roomie/services/profile_service/entity/UserProfile.java
@@ -0,0 +1,56 @@
+package com.roomie.services.profile_service.entity;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import com.roomie.services.profile_service.enums.AccountStatus;
+import com.roomie.services.profile_service.enums.Gender;
+import org.springframework.data.neo4j.core.schema.GeneratedValue;
+import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.Node;
+import org.springframework.data.neo4j.core.schema.Property;
+import org.springframework.data.neo4j.core.support.UUIDStringGenerator;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Node("user_profile")
+public class UserProfile {
+    @Id
+    @GeneratedValue(generatorClass = UUIDStringGenerator.class)
+    String id;
+
+    @Property("userId")
+    String userId;
+
+    String avatar;
+
+    String username;
+
+    @Property("email")
+    String email;
+
+    @Property("phoneNumber")
+    String phoneNumber;
+
+    @Property("firstName")
+    String firstName;
+
+    @Property("lastName")
+    String lastName;
+
+    Gender gender;
+
+    LocalDate dob;
+    String location;
+
+    AccountStatus status;
+
+    LocalDateTime updatedAt;
+}
