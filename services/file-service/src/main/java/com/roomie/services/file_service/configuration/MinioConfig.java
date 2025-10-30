6af693bf2ac6c60058174c6863f8a70c8fb27b23
lowjichow <phucloidanghoconline@gmail.com>
2025-10-31 02:38:37 +0700

    add configuration

---
+++ services/file-service/src/main/java/com/roomie/services/file_service/configuration/MinioConfig.java
@@ -0,0 +1,34 @@
+package com.roomie.services.file_service.configuration;
+
+import io.minio.MinioClient;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Setter
+@Configuration
+public class MinioConfig {
+
+    @Value("${app.minio.endpoint}")
+    private String endpoint;
+
+    @Value("${app.minio.access-key}")
+    private String accessKey;
+
+    @Value("${app.minio.secret-key}")
+    private String secretKey;
+
+    @Value("${app.minio.bucket}")
+    private String bucket;
+
+    @Bean
+    public MinioClient minioClient() {
+        return MinioClient.builder()
+                .endpoint(endpoint)
+                .credentials(accessKey, secretKey)
+                .build();
+    }
+}
